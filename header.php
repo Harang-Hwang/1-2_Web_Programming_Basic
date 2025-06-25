eadedc81bd7a8ac418990ef4a125ca24a03faeab
Harang Hwang <haranghwang@gmail.com>
2025-06-25 18:24:53 +0900

    웹 프로그래밍 기초 기말 프로젝트

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="1.css">
+    <script src="https://kit.fontawesome.com/def66b134a.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <h1>WITH SKY &nbsp; </h1>
+
+    <input class="box" type="text" class="search" src="search.png" alt="" value="'떠나고 싶은 여행지를 검색해보세요!'">
+
+    <h2>
+        MY
+        &nbsp;
+        <img class="accountimg" src="account.png" alt="">
+        &nbsp;
+        MENU
+    </h2>
+
+    <ul class="menu">
+        <li>
+            <a href="introduce.php">01. Introduce</a></li>
+        <li>
+            <a href="package.php">02. Package</a></li>
+        <li>
+            <a href="flight.php">03. Flight</a></li>
+    </ul>
---
+++ intro.php
@@ -0,0 +1,78 @@
+<? include_once("header.php");?>
+
+<h2>
+        이달의 추천 여행지 : 프랑스
+    </h2>
+
+    <div>
+<img class="Eiffel" src="Eiffel Tower Sunset.jpg" alt="">
+</div>
+
+<p class="t">
+    프랑스는 유럽 대륙의 서부와 지중해와 대서양 사이에 위치하며, 유럽에서 3번째로 큰 나라이다.
+    프랑스의 수도는 파리로, 유명 관광지인 '에펠탑(Tour Eiffe)'이 위치해있다.
+    이 외에도 루브르 박물관(Musee du Louvre), 베르사유 궁전(Chateau de Versailles), 개선문(Arc de Triomphe), 오르세 미술관(Musee d’Orsay),
+    샹젤리제 거리(Avenue des Champs-Elysees), 노틀담 성당(Cathedrale Notre Dame de Paris), 몽생미셸 수도원(Abbaye du Mont-Saint-Miche) 등
+    여러 관광지가 있다.
+    특히, 샹젤리제 거리에서는 매년 7월 프랑스에서 개최되는 세계 최고 권위의 일주 사이클 대회인 '투르 드 프랑스(Tour de France)'를 시행하기도 해
+    7월에 프랑스 샹젤리제 거리를 방문하는 관광객은 투르 드 프랑스를 관람할 수 있다.
+</p>
+
+<hr>
+    
+<div>
+<h2>
+    프랑스 숙소 추천
+</h2>
+
+<p class="u">
+    프랑스의 여러 숙소들을 추천해드립니다!
+    리뷰와 함께 둘러보시면서 마음에 드는 숙소를 북마크해보세요!
+</p>
+
+</div>
+
+<img class="x" src="메인 숙소.png" alt="">
+<img class="x" src="숙소 3.png" alt="">
+<img class="x" src="숙소 2.png" alt="">
+
+<img class="bm" src="bookmark.png" alt="">
+
+<p>
+    Waldorf Astoria Versailles - Trianon Palace
+    REVIEW ⭐⭐⭐⭐⭐
+    "최고급인 5성급 값 하는 호텔! 정말 깔끔하고 편리하며, 직원분들도 너무 친절하셔서 지내는 동안 정말 좋았어요!"
+</p>
+
+<hr>
+
+<h2>
+    프랑스 맛집 추천
+</h2>
+<div>
+    <img class="i" src="프랑스 현지 맛집 루루 1.png" alt="">
+    <img class="i" src="프랑스 현지 맛집 루루 2.png" alt="">
+    <img class="i" src="프랑스 현지 맛집 루루 3.png" alt="">
+    <img class="i" src="프랑스 현지 맛집 루루 4.png" alt="">
+<p>
+    루루 (Loulou)
+    프랑스 파리의 에펠탑, 루브르 박물관 인근에 위치한 현지 맛집으로, 큰 규모의 레스토랑이다.
+    야외와 실내 모두 좌석이 있으며, 실내는 바를 겸하고 있어 약간 어두운 분위기이다.
+    이 식당을 다녀간 관광객들은 루브르 박물관에서 작품들을 관람하고 점심으로 먹기 좋았다고 한다.
+    이 식당의 특징은 야외석의 일부를 예약했을 때, 에펠탑을 보며 식사를 할 수 있다는 점이다.</p>
+</div>
+<hr>
+
+<h2>
+    주변 여행지 소개
+</h2>
+
+<div>
+<img src="프랑스 지도.png" alt="">
+</div>
+
+<p>
+    주변 여행지 이름 & 소개 글 적기!
+</p>
+
+<? include_once("footer.php"); ?>
